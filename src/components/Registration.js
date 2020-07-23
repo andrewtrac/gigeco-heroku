720ffc7157d1085e06e2bbbaf47679047ef7d3ab
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-23 03:38:08 -0400

    added memberstack

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Login() {
 
+  useEffect(() => {
+    let script = document.createElement('script');
+    script.src = 'https://api.memberstack.io/static/memberstack.js?custom';
+    script.setAttribute('data-memberstack-id', '335d53e2477f259d83f9b05ece497148');
+    script.async = true;
+    script.onload = function () {
+      console.log('Memberstack Loaded')
+    }
+    document.head.appendChild(script);
+  }, [])
+
   return (
-    <h1>Registration</h1>
-
+    <form data-ms-form="signup">
+      <input ms-field="email" />
+      <input ms-field="password" />
+      <input type="submit" value="Sign up"/>
+    </form>
   )
-
 }
