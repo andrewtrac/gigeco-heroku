ddb0697794806e35b41545076eb9579099aedb4d
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-23 03:50:30 -0400

    added login and logout button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Login() {
 
-  return (
-    <h1>Login</h1>
+  useEffect(() => {
+    let script = document.createElement('script');
+    script.src = 'https://api.memberstack.io/static/memberstack.js?custom';
+    script.setAttribute('data-memberstack-id', '335d53e2477f259d83f9b05ece497148');
+    script.async = true;
+    script.onload = function () {
+      console.log('Memberstack Loaded')
+    }
+    document.head.appendChild(script);
+  }, [])
 
+
+  return (
+    <form data-ms-form="login">
+    <input data-ms-member="email" />
+    <input data-ms-member="password" />
+    <input type="submit" value="Login"/>
+  </form>
   )
+
 
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Welcome() {
 
   return (
+    <>
     <h1>Welcome</h1>
+    <input type="button" onclick="location.href='/#/ms/logout" value="Logout" />
+    </>
 
   )
 
