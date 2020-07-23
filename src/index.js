bb5c53987a3d2624c0a898cea7ced9c104386a45
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-23 03:32:07 -0400

    Initialized routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom'
 import App from './App';
+import Login from './components/Login'
+import Registration from './components/Registration'
+import Welcome from './components/Welcome'
+import Notfound from './components/Notfound'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+    <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/registration">Registration</Link>
+        </li>
+      </ul>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/registration" component={Registration} />
+      <Route path="/welcome" component={Welcome} />
+      <Route component={Notfound} />
+    </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render( routing, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
